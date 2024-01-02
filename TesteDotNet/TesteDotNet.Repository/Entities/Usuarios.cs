d793af1e0dc3cc175a066b75d2cd9a7cec43a815
Ivan Pedro Almeida Queiroz <pedroalmeida013@hotmail.com>
2024-01-02 00:04:58 -0300

    [FIX]  Mapeando tabela de usuarios

---
+++ TesteDotNet/TesteDotNet.Repository/Entities/Usuarios.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace TesteDotNet.Repository.Entities
+{
+    public class Usuarios
+    {
+        [Key]
+        public int id { get; set; }
+        public string usuario { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public string cpf { get; set; }
+        public string email { get; set; }
+        public long? telefone { get; set; }
+        public long? celular { get; set; }
+        public bool isWhatsApp { get; set; }
+        public DateTime dataInclusao { get; set; }
+        public DateTime? dataAlteracao { get; set; }
+        public DateTime? dataExclusao { get; set; }
+        public bool isExcluido { get; set; }
+    }
+}
