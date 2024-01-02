2df2e20ed0337d1cdbb4430c4bea943aed19737c
Ivan Pedro Almeida Queiroz <pedroalmeida013@hotmail.com>
2024-01-02 00:28:02 -0300

    [FIX] Implementando DependecyInjection

---
+++ TesteDotNet/TesteDotNet.Web/DependecyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using TesteDotNet.Repository.Context;
+
+namespace TesteDotNet.Web
+{
+    public static class DependecyInjection
+    {
+        public static WebApplicationBuilder ConfigureDI(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<UsuarioContext>(options =>
+             options.UseSqlServer(builder.Configuration.GetConnectionString("UsuariosConnectionString")));
+
+            return builder;
+        }
+      
+    }
+}
--- TesteDotNet/TesteDotNet.Web/Program.cs
+++ TesteDotNet/TesteDotNet.Web/Program.cs
@@ -1,7 +1,10 @@
+using TesteDotNet.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.ConfigureDI();
 
 var app = builder.Build();
 
