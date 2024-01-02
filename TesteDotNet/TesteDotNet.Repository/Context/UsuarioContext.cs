5571d338702acd32df8e967adacda3a69d4945ec
Ivan Pedro Almeida Queiroz <pedroalmeida013@hotmail.com>
2024-01-02 00:16:33 -0300

    [FIX] Implementando context na aplicação

---
+++ TesteDotNet/TesteDotNet.Repository/Context/UsuarioContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using TesteDotNet.Repository.Entities;
+
+namespace TesteDotNet.Repository.Context
+{
+    public class UsuarioContext : DbContext 
+    {
+        public UsuarioContext(DbContextOptions<UsuarioContext> options) : base (options) 
+        {
+        }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+    }
+}
