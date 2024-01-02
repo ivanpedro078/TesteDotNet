3f488cbd82ed0346499d97fab10d46a59d423f04
Ivan Pedro Almeida Queiroz <pedroalmeida013@hotmail.com>
2024-01-02 00:39:22 -0300

    [FIX] Implementando a tabela de usuarios utilizando o DTO

---
+++ TesteDotNet/TesteDotNet.Business/DTOs/UsuarioDTO.cs
@@ -0,0 +1,36 @@
+﻿using TesteDotNet.Repository.Entities;
+
+namespace TesteDotNet.Business.DTOs
+{
+    public class UsuarioDTO
+    {
+        public int id { get; set; }
+        public string usuario { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public string cpf { get; set; }
+        public string email { get; set; }
+        public long? telefone { get; set; }
+        public long celular { get; set; }
+        public bool isWhatsApp { get; set; }
+        public DateTime dataInclusao { get; set; }
+        public DateTime? dataAlteracao { get; set; }
+        public DateTime? dataExclusao { get; set; }
+        public bool isExcluido { get; set; }
+
+        public UsuarioDTO(Usuarios usuarios)
+        {
+            id = usuarios.id;
+            usuario = usuarios.usuario;
+            dataNascimento = usuarios.dataNascimento;
+            cpf = usuarios.cpf;
+            email = usuarios.email;
+            telefone = usuarios.telefone;
+            celular = usuarios.celular;
+            isWhatsApp = usuarios.isWhatsApp;
+            dataInclusao = usuarios.dataInclusao;
+            dataAlteracao = usuarios.dataAlteracao;
+            dataExclusao = usuarios.dataExclusao;
+            isExcluido = usuarios.isExcluido;
+        }
+    }
+}
