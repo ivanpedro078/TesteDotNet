d5dcccd94e36cc458d8723a1bcd03a7c559c0df9
Ivan Pedro Almeida Queiroz <pedroalmeida013@hotmail.com>
2024-01-02 23:26:32 -0300

    [FIX] Reduzindo linhas desnecessárias no contrutor no DTO

--- TesteDotNet/TesteDotNet.Business/DTOs/UsuarioDTO.cs
+++ TesteDotNet/TesteDotNet.Business/DTOs/UsuarioDTO.cs
@@ -4,33 +4,24 @@ namespace TesteDotNet.Business.DTOs
 {
     public class UsuarioDTO
     {
-        public int id { get; set; }
         public string usuario { get; set; }
-        public DateTime dataNascimento { get; set; }
+        public string dataNascimento { get; set; }
         public string cpf { get; set; }
         public string email { get; set; }
         public long? telefone { get; set; }
         public long celular { get; set; }
         public bool isWhatsApp { get; set; }
-        public DateTime dataInclusao { get; set; }
-        public DateTime? dataAlteracao { get; set; }
-        public DateTime? dataExclusao { get; set; }
-        public bool isExcluido { get; set; }
+
 
         public UsuarioDTO(Usuarios usuarios)
         {
-            id = usuarios.id;
             usuario = usuarios.usuario;
-            dataNascimento = usuarios.dataNascimento;
+            dataNascimento = usuarios.dataNascimento.ToString("dd/mm/yyyy");
             cpf = usuarios.cpf;
             email = usuarios.email;
             telefone = usuarios.telefone;
             celular = usuarios.celular;
             isWhatsApp = usuarios.isWhatsApp;
-            dataInclusao = usuarios.dataInclusao;
-            dataAlteracao = usuarios.dataAlteracao;
-            dataExclusao = usuarios.dataExclusao;
-            isExcluido = usuarios.isExcluido;
         }
     }
 }
---
+++ TesteDotNet/TesteDotNet.Business/Services/UsuarioService.cs
@@ -0,0 +1,7 @@
+﻿namespace TesteDotNet.Business.Services
+{
+    public class UsuarioService
+    {
+
+    }
+}
